2e63c26b63d78afd56858f5c06b8b5708faeea8b
Yongeun Kwon <ykwon35@my.bcit.ca>
2023-08-06 23:41:13 -0700

    Week 8 Solutions

---
+++ LeetCode-Yongeun/328. Odd Even Linked List.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+    ListNode oddHead = new ListNode(0);
+    ListNode evenHead = new ListNode(0);
+    ListNode odd = oddHead;
+    ListNode even = evenHead;
+
+    for (boolean isOdd = true; head != null; head = head.next, isOdd = !isOdd)
+      if (isOdd) {
+        odd.next = head;
+        odd = odd.next;
+      } else {
+        even.next = head;
+        even = even.next;
+      }
+
+    odd.next = evenHead.next;
+    even.next = null;
+    return oddHead.next;
+  }
+}
